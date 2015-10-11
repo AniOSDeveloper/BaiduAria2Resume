b8a4f26b7e8d90545ff8d27d1bb2a6f33f331fa5
Sun <sunzy2@gmail.com>
2015-10-11 18:56:07 +0800

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # BaiduAria2Resume
-百度的链接有实效性，这个脚本就是用来修复实效链接的。
+
+##总述
+百度的链接有时效性，保持这个脚本在后台运行，修复失效链接。
+
+
+##说明
+百度网盘的下载速度是有目共睹的，配合aria2下载一般都可以达到满速
+推荐配合插件:
+
+###BaiduExporter
+<https://github.com/acgotaku/BaiduExporter>
+
+可以方便的把百度云任务添加到aria2中。
+
+
+###webui-aria2
+<https://github.com/ziahamza/webui-aria2>
+
+aria2 比较好的管理页面
+
+##参数
+
+```
+➜  BaiduAria2Resume git:(master) ✗ python baidu.py -h
+usage: baidu.py [-h] -u UserName -p Password [-port Port] [-host HOST] [-r]
+                [-i Id]
+
+optional arguments:
+  -h, --help   show this help message and exit
+  -u UserName  Baidu User Name
+  -p Password  Baidu Password
+  -port Port   Aria2 server port,default:6800
+  -host HOST   Aria2 host address,default:127.0.0.1
+  -r           Refresh cache,shoule be true when file changes on pcs.
+  -i Id        Aria2 download id #设定的aria2 id，每个用户不同，BaiduExporter用的是“aria2_download”，也是脚本的默认值.
+
+```
+####Tips
+第一次使用会扫一遍百度云中的所有文件，速度比较慢。加了cache，第二次就不会扫了。但是如果百度云中的文件有变化，请加上 ”-r“ 选项刷新cache
+
+
+####Example
+```
+python baidu.py -u username -p passwd -host 127.0.0.1 -port 6800 -r -i aria2_download
+```
+
+
+##注意
+因为是配合BaiduExporter使用的，所以本地下载文件的相对路径必须要和百度云中的一致，否则无法使用。
+
---
+++ baidu.py
@@ -0,0 +1,171 @@
+# coding=utf-8
+import base64
+import json
+import re
+import login
+from time import time
+import urllib
+import sys
+import urllib2
+import hashlib
+import pickle
+import os
+import argparse
+
+reload(sys)
+sys.setdefaultencoding("utf8")
+
+
+class BaiduResume:
+    def __init__(self, user_name, passwd, host="127.0.0.1", port=6800, aria2_id="aria2_download", refresh=False):
+        self.user_name = user_name
+        self.passwd = passwd
+        self.host = host
+        self.port = port
+        self.list_url = "http://pan.baidu.com/api/list?channel=chunlei&clienttype=0&web=1&num=100&page=1&order=time&desc=1&showempty=0&channel=chunlei&clienttype=0&web=1&"
+        self.download_url = "http://pan.baidu.com/api/download?type=dlink&channel=chunlei&clienttype=0&web=1&"
+        self.account = login.login([self.user_name, self.passwd])
+        self.session = self.account.session
+        self.all_file_fid = {}  # {filename_hash:[fid,path]}
+        self.aria2_url = 'http://%s:%d/jsonrpc' % (self.host, self.port)
+        self.aria2_id = aria2_id
+        self.aria2_header = "Cookie:BDUSS=" + self.account.bduss
+        self.cache_path = os.path.expanduser("~/.baidu.cache")
+        self.refresh = refresh
+
+    def _get_all_file_fid(self, path):
+        list_params = urllib.urlencode({"dir": path, "_": int(time()), "bdstoken": self.account.token})
+        response = self.session.get(self.list_url + list_params)
+        list_data = json.loads(response.text)
+        for sub_path in list_data.get("list"):
+            sub_path_name = sub_path['server_filename']
+            if sub_path['isdir']:
+                if not sub_path['empty']:
+                    self._get_all_file_fid(path + sub_path_name + "/")
+            else:
+                fs_id = sub_path['fs_id']
+                self.all_file_fid[get_hash_code(path + sub_path_name)] = fs_id
+
+    def _aria2_rpc_error_task(self):
+        jsonreq = json.dumps({'jsonrpc': '2.0', "id": self.aria2_id,
+                              'method': 'aria2.tellStopped', 'params': [0, 65536, ['files', 'dir', 'status', 'gid']]})
+        res = urllib2.urlopen(self.aria2_url, jsonreq).read()
+        all_stoped = json.loads(res).get("result")
+        if all_stoped:
+            for item in all_stoped:
+                if item.get("status") == "error":
+                    dl_dir = item.get("dir")
+                    gid = item.get("gid")
+                    path = item.get("files")[0].get("path")
+                    baidu_path = path.replace(dl_dir, "")
+                    result = self._aria2_rpc_add_uri(self._get_download_link(baidu_path),
+                                                     self.aria2_header, dl_dir, baidu_path[1:])
+                    self._aria2_rpc_remove(gid)
+
+    def _aria2_rpc_add_uri(self, dlink, header, dl_dir, out):
+        jsonreq = json.dumps({'jsonrpc': '2.0', "id": self.aria2_id,
+                              'method': 'aria2.addUri', 'params': [[dlink],
+                                                                   {'header': header, 'dir': dl_dir,
+                                                                    'out': out}]})
+        res = urllib2.urlopen(self.aria2_url, jsonreq)
+        return json.loads(res.read())
+
+    def _aria2_rpc_remove(self, gid):
+        jsonreq = json.dumps({'jsonrpc': '2.0', 'id': self.aria2_id, 'method': 'aria2.removeDownloadResult',
+                              'params': [str(gid)]})
+        res = urllib2.urlopen(self.aria2_url, jsonreq)
+        return json.loads(res.read())
+
+    def _get_download_link(self, baidu_path):
+        url = self.download_url + urllib.urlencode(
+            {"timestamp": int(time()), "fidlist": "[" + str(self.all_file_fid[get_hash_code(baidu_path)]) + "]",
+             "bdstoken": self.account.token,
+             "sign": self._get_sign()})
+        response = self.session.get(url)
+        json_data = json.loads(response.text)
+        if json_data['errno'] == 0:
+            dlink = json_data.get('dlink')[0].get('dlink')
+            return dlink
+        else:
+            print response.text
+        return None
+
+    def _get_sign(self):
+        res = self.session.get("http://pan.baidu.com/disk/home").content
+        sign1 = re.search(r'yunData.sign1\s=\s\'(.*?)\';', res).group(1)
+        sign3 = re.search(r'yunData.sign3\s=\s\'(.*?)\';', res).group(1)
+        timestamp = re.search(r'timestamp = \'(.+?)\';', res).group(1)
+
+        def sign2(j, r):
+            a = []
+            p = []
+            o = ''
+            v = len(j)
+            for q in xrange(256):
+                a.append(ord(j[(q % v)]))
+                p.append(q)
+
+            u = 0
+            for q in xrange(256):
+                u = (u + p[q] + a[q]) % 256
+                p[q], p[u] = p[u], p[q]
+
+            i = 0
+            u = 0
+            for q in xrange(len(r)):
+                i = (i + 1) % 256
+                u = (u + p[i]) % 256
+                p[i], p[u] = p[u], p[i]
+                k = p[((p[i] + p[u]) % 256)]
+                o += chr(ord(r[q]) ^ k)
+            return base64.b64encode(o)
+
+        return sign2(sign3, sign1)
+
+    def _scan_save_all_file_fid(self):
+        print("Sanning all pcs files...")
+        self._get_all_file_fid("/")
+        print("Scanning finished,save to cache...")
+        with open(self.cache_path, "w") as f:
+            pickle.dump(self.all_file_fid, f)
+        print("Cache save to " + self.cache_path)
+
+    def start(self):
+        print("Looking for fid cache...")
+        if os.path.exists(self.cache_path) and not self.refresh:
+            print("Try load cache...")
+            try:
+                with open(self.cache_path, "r") as f:
+                    self.all_file_fid = pickle.load(f)
+                    print("Load cache success!!!")
+            except Exception as e:
+                print("Cache is empty...")
+                self._scan_save_all_file_fid()
+        else:
+            self._scan_save_all_file_fid()
+
+        print self._aria2_rpc_error_task()
+
+
+def get_hash_code(s):
+    key = hashlib.md5()
+    key.update(s)
+    return key.hexdigest()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-u", dest="username", metavar="UserName", required=True, help="Baidu User Name")
+    parser.add_argument("-p", dest="passwd", metavar="Password", required=True, help="Baidu Password")
+    parser.add_argument("-port", dest="port", metavar="Port", default=6800, type=int, required=False,
+                        help="Aria2 server port,default:6800")
+    parser.add_argument("-host", dest="host", default="127.0.0.1", help="Aria2 host address,default:127.0.0.1")
+    parser.add_argument("-r", dest="refresh", default=False, action='store_true',
+                        help="Refresh cache,shoule be true when file changes on pcs.")
+    parser.add_argument("-i", dest="aria2_id", metavar="Id", default="aria2_download",
+                        help="Aria2 download id.default:aria2_download")
+    args = parser.parse_args()
+
+    resume = BaiduResume(user_name=args.username, passwd=args.passwd, host=args.host,
+                         port=args.port, aria2_id=args.aria2_id)
+    resume.start()
