b87c2603b2894676f5a4a01e440579f9bbc362fd
Sun <sunzy2@gmail.com>
2015-10-14 00:17:38 +0800

    增加一个小玩意

---
+++ control.py
@@ -0,0 +1,44 @@
+# coding:utf-8
+import socket
+import urllib2
+import json
+import os
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tv_open = False
+status_file = "/tmp/tv_open"
+if os.path.exists(status_file):
+    tv_open = True
+
+
+def pause_all():
+    aria2_call("aria2.pauseAll")
+
+
+def resume_all():
+    aria2_call("aria2.unpauseAll")
+
+
+def aria2_call(method):
+    jsonreq = json.dumps({'jsonrpc': '2.0', 'id': 'aria2_download', 'method': method, 'params': []})
+    response = urllib2.urlopen("http://192.168.0.36:6800/jsonrpc", jsonreq)
+
+
+try:
+    s.connect(('192.168.0.36', 22))
+    if tv_open:
+        pass
+    else:
+        print("resume all")
+        resume_all()
+        with open(status_file, 'w') as f:
+            f.write("on")
+except socket.error as e:
+    if not tv_open:
+        pass
+    else:
+        pause_all()
+        print "pause all"
+        os.remove(status_file)
+finally:
+    s.close()
