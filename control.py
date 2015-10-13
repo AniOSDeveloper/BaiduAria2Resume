a9344e8a462782e410ab837f624f28109f7a8941
Sun <sunzy2@gmail.com>
2015-10-14 01:24:16 +0800

    增加一个小玩意

--- control.py
+++ control.py
@@ -6,10 +6,11 @@ import os
 import sys
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tv_open = False
+s.settimeout(2)
+aria_stop = False
 status_file = "/tmp/tv_open"
 if os.path.exists(status_file):
-    tv_open = True
+    aria_stop = True
 
 host_ip = sys.argv[1]
 if not host_ip:
@@ -31,19 +32,19 @@ def aria2_call(method):
 
 try:
     s.connect((host_ip, 22))
-    if tv_open:
-        pass
-    else:
-        print("resume all")
-        resume_all()
+    if not aria_stop:
+        print("pause all")
+        pause_all()
+
+    if not os.path.isfile(status_file):
         with open(status_file, 'w') as f:
             f.write("on")
+
 except socket.error as e:
-    if not tv_open:
-        pass
-    else:
-        pause_all()
-        print "pause all"
+    if aria_stop:
+        resume_all()
+        print "resume all"
         os.remove(status_file)
 finally:
     s.close()
+
